b2a4d2c6908f0300252f8f09107a20ef22c430a3
jsampada <joshisampada.03@gmail.com>
2022-09-26 15:25:24 +0530

    list practice program

---
+++ Name.py
@@ -0,0 +1,18 @@
+id's'=[,"GHRS-1002","GHRS-1003","GHRS-1004"]
+lst2=["sampada","rohini","aditi","siya","jiya"]
+proId=["GHRP-1001"]
+list_users =[
+    {
+        'ID' : "GHRS-1001",
+        'first_name' :'Sampada',
+        'last_name':'Joshi'
+    },
+    {
+        'ID' : "GHRS-1001",
+        'first_name' :'Sampada',
+        'last_name':'Joshi'
+    },
+]
+
+lst3= dict(zip(lst1,lst2))
+print(lst3)
